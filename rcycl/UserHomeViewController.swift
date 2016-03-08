14c3d9f01a2865e8aace784a7eb1e65bcf509114
Joey Poon <joey@joeypoon.com>
2016-03-07 23:24:19 -0600

    create pickup

--- rcycl/UserHomeViewController.swift
+++ rcycl/UserHomeViewController.swift
@@ -14,8 +14,24 @@ class UserHomeViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
     }
-
     @IBAction func schedulePickupPressed(sender: UIButton) {
+        let parameters = [
+            "pickup": [
+                "user_id": session.id,
+                "time": NSDate()
+            ]
+        ]
         
+        let headers = [
+            "x-auth-token": session.auth_token
+        ]
+        
+        Alamofire.request(.POST, "\(session.rootURL)pickups", parameters: parameters, headers: headers)
+            .responseJSON { response in
+                guard let json = response.result.value else {
+                    return
+                }
+                print(json["pickup"])
+        }
     }
 }
